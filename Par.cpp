f1b9931701337dc5e0c67b015bf7c1da6409d489
glayberson <glaybersonsilva@outlook.com>
2023-08-19 11:12:11 -0300

    Feito com função e sem função comentada

---
+++ Par.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int par(){
+    int num, som = 0;
+    
+   
+    while(num > 0){
+        cout << "Digite um numero negativo para parar \n";
+        cin >> num;
+        if(num % 2 == 0){
+            som = som + num;
+        }
+       
+    } 
+    return som;
+}
+
+
+int main (){
+    int num, som = 0;
+    cout << " Digite numeros inteiros \n";
+    cin >> num;
+     if(num % 2 == 0){
+        som = num;
+    }
+    // while(num > 0){
+    //     cout << "Digite um numero negativo para parar \n";
+    //     cin >> num;
+    //     if(num % 2 == 0){
+    //         som = som + num;
+    //     }
+    //     cout << " O resultado :" << som << '\n';
+    // }
+    cout << par() + som ;
+}
