a24c1f9a80bd6dc6412f36af98b8f9fef0790421
glayberson <glaybersonsilva@outlook.com>
2023-08-24 19:54:08 -0300

    Problema resolvido

--- inverter_vetor.cpp
+++ inverter_vetor.cpp
@@ -3,23 +3,29 @@
 using namespace std;
 
 int main (){
-    double v[7];
+    cout << "Digite o tamanho do vetor \n ";
+    int m;
+    cin >> m;
+    
+    double v[m];
     double j;
-    int n = 6;
-    for (int i = 0; i < 7; i++){
+    int n = m - 1;
+    for (int i = 0; i < m; i++){
         v[i] = i + 0.1;
         
     }
-    for (int i = 0; i < 7; i++) {
+    for (int i = 0; i < m; i++) {
         cout << "o " << i << " numero do vetor = " << v[i] << ('\n');
     }
-    for (int i = 0; i < 7; i++) {
+    for (int i = 0; i < m/2; i++) {
         j = v[i];
-        v[i] = v[n - i];
-        v[n - i] = j;
-
+        v[i] = v[n];
+        v[n] = j;
+        n--;
+    }
+    
+   for (int i = 0; i < m; i++) {
         cout << "Os " << i << " numeros do vetor = " << v[i] << ('\n');
     }
-   
 
 }
