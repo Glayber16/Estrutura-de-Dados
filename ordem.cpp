5f02f78f337a9e88438f71474f00228e51e0b015
glayberson <glaybersonsilva@outlook.com>
2023-08-28 13:56:46 -0300

    Ordem implementada

---
+++ ordem.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void ordena (double *v, int n){
+    int maior;
+    for(int i = 0; i < n; i++){
+        for(int j = i; j < n; j++){
+            if(v[i] > v[j]){
+                maior = v[i];
+                v[i] = v[j];
+                v[j] = maior;
+            }
+        }
+    }
+}
+
+int main (){
+    int n;
+    cout << "Digite o tamanho do vetor \n";
+    cin >> n;
+    double v[n];
+    for (int i = 0; i < n; i++){
+        cout << "Digite os valores do vetor \n";
+        cin >> v[i];
+    }
+    ordena (v, n);
+    for (int i = 0; i < n; i++){
+        cout << v[i] << "\n";
+        
+    }  
+
+}
